c921cd8850d9aa9e907e330f210e9f7c8ec103dc
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-08-07 09:57:01 -0700

    Add initial files for word counter application

---
+++ lib/word_counter.rb
@@ -0,0 +1,7 @@
+class String
+  define_method(:word_counter) do |user_word|
+    input_word = self
+    input_word = user_word
+
+  end
+end
---
+++ spec/word_counter_specs.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('word_counter')
+
+describe('String#word_counter') do
+  it("Correctly identifies the inputed word") do
+    expect(("dog".word_counter())to.(eq("dog"))
+  end
+end
